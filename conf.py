4f9205a717c9a26dd34cf194460e85025593f86b
Jeremy Avigad <avigad@cmu.edu>
2017-07-26 16:04:55 +0100

    fix(conf.py): add Gabriel and Sebastian as authors

--- conf.py
+++ conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 # General information about the project.
 project = 'The Lean Reference Manual'
 copyright = '2017, Jeremy Avigad'
-author = 'Jeremy Avigad'
+author = 'Jeremy Avigad, Gabriel Ebner, Sebastian Ullrich'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
