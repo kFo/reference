b496dd3e31f5febbb64aa49f321a929d6261723e
Jeremy Avigad <avigad@cmu.edu>
2017-08-14 18:13:31 -0400

    feat(conf.py,_static/favicon.ico): add L favicon

--- conf.py
+++ conf.py
@@ -114,6 +114,8 @@ html_theme_options = {
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_favicon = '_static/favicon.ico'
+
 
 # -- Options for HTMLHelp output ------------------------------------------
 
